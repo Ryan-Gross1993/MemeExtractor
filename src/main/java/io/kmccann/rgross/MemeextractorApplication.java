cea329381297ca8e2f6d782f19429ea7e86e3296
Ryan Gross <ryan@Ryans-MacBook-Pro.local>
2017-03-28 11:12:47 -0400

    Base for extractor

---
+++ src/main/java/io/kmccann/rgross/MemeextractorApplication.java
@@ -0,0 +1,12 @@
+package io.kmccann.rgross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeextractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeextractorApplication.class, args);
+	}
+}
